8b971c434edd1ad87b7f32c6564a9dec3138736b
Shunham Singh <73241805+Shubham11994@users.noreply.github.com>
2022-09-05 15:33:48 +0530

    Update Compare.java

--- String/Compare.java
+++ String/Compare.java
@@ -1,1 +1,24 @@
+/* s1 = hello s2= java   output = s1 less than s2    hint = h<j
+   s1 = java s2 = hello  output = s1 greater than s2 hint = h<j   */
 
+
+
+import java.util.Scanner;	
+	class Compare{
+
+		public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			String S1 = sc.next();     // A>B>C.....>Z
+			String S2 = sc.next();     // a>b>c>....>z
+			
+			int result = S1.compareTo(S2);
+		    if(result<0){
+				System.out.print("S1 is less than S2");
+			}else
+			if(result==0){
+				System.out.print("S1 equal to S2");
+			}else
+				System.out.print("S1 is greater than S2");
+	    }
+    }
+	
